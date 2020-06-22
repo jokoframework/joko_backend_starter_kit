ef8993194da2063b9f84b9d50be668b9e2170c84
Ale Feltes <afeltes@sodep.com.py>
2020-06-22 16:19:06 -0400

    Aplica update de seguridad
    - Convierte el proyecto a un jar ejecutable con tomcat embedded.

--- src/main/java/io/github/jokoframework/myproject/Application.java
+++ src/main/java/io/github/jokoframework/myproject/Application.java
@@ -3,8 +3,6 @@ package io.github.jokoframework.myproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -17,12 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaRepositories(basePackages = {"io.github.jokoframework"})
 @EntityScan(basePackages = {"io.github.jokoframework"})
 @EnableScheduling
-public class Application extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
